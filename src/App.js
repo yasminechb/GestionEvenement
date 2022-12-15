ec4810c16eddd03901810134182cf8269cd05b19
yasminechb <chebbiyasmine9@gmail.com>
2022-12-15 21:48:02 +0100

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>this is my first react app</h1>
+      <p> i am learning react rabi meaa e rojla </p>
     </div>
+    
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import "./NavbarStyles.css" ; //import the style sheet specific to this component 
+
+class Navbar extends Component { // we extend men class component maaneha nekhdou menha f hajet 
+    render() {  // we using render because we extends if you have to exdend a coponent you have to render it  
+return(           //inside this return we will start creating our navbar 
+    <nav className="NavbarItems"> 
+     <h1 className="N"></h1>
+    </nav>  // nav bar items aana logo ,
+)
+    }  
+}
